5a151339c81040729b62d0ea00e973624bd34edb
sabibulaev25@gmail.com <sabibulaev25@gmail.com>
2023-04-14 21:20:18 +0300

    Update

---
+++ OOP2_Task4/OOP2_Task4/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OOP2_Task4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rows1, columns1;
+            string str_row = textBox1.Text;
+            string str_col = textBox2.Text;
+            bool check1 = int.TryParse(str_row, out rows1);
+            bool check2 = int.TryParse(str_col, out columns1);
+            if (check1 && check2)
+            {
+                dataGridView1.RowCount = rows1;
+                dataGridView1.ColumnCount = columns1;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int rows2, columns2;
+            string str_row = textBox3.Text;
+            string str_col = textBox4.Text;
+            bool check1 = int.TryParse(str_row, out rows2);
+            bool check2 = int.TryParse(str_col, out columns2);
+            if (check1 && check2)
+            {
+                dataGridView2.RowCount = rows2;
+                dataGridView2.ColumnCount = columns2;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int ii = 0; ii < dataGridView1.ColumnCount; ii++)
+                {
+                    dataGridView1.Rows[i].Cells[ii].Value = Convert.ToDouble(rnd.Next(-10000, 10000)) / 100;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                for (int ii = 0; ii < dataGridView2.ColumnCount; ii++)
+                {
+                    dataGridView2.Rows[i].Cells[ii].Value = Convert.ToDouble(rnd.Next(-10000, 10000)) / 100;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView3.RowCount = dataGridView1.RowCount;
+                dataGridView3.ColumnCount = dataGridView2.ColumnCount;
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                    {
+                        double element = 0;
+                        for (int ii = 0; ii < dataGridView1.ColumnCount; ii++)
+                        {
+                            element +=
+                                Convert.ToDouble(dataGridView1.Rows[i].Cells[ii].Value) *
+                                Convert.ToDouble(dataGridView2.Rows[ii].Cells[j].Value);
+                        }
+                        dataGridView3.Rows[i].Cells[j].Value = element;
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException ex) { MessageBox.Show("Ошибка: " + ex.Message); }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 0;
+            dataGridView1.ColumnCount = 0;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            dataGridView2.RowCount = 0;
+            dataGridView2.ColumnCount = 0;
+        }
+    }
+}
